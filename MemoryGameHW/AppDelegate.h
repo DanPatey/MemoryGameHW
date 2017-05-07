1f4efbeba8ddbd49b4ee55a6af334fda88809bfd
Dan Patey <danpatey@danpatey.com>
2017-05-07 15:06:46 -0700

    Initial Commit

---
+++ MemoryGameHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MemoryGameHW
+//
+//  Created by Dan Patey on 5/7/17.
+//  Copyright © 2017 Dan Patey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MemoryGameHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemoryGameHW
+//
+//  Created by Dan Patey on 5/7/17.
+//  Copyright © 2017 Dan Patey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
