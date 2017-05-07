aa1c458b86f69bb66dcb7ec1c1a08c287597bfcf
Dan Patey <danpatey@danpatey.com>
2017-05-07 16:21:46 -0700

    prototyping images; setting up views and buttons

--- MemoryGameHW/ViewController.m
+++ MemoryGameHW/ViewController.m
@@ -8,21 +8,59 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
 
+NSArray *imgsArr;
+
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self arrayMakeAction];
+    
+    CGRect testImgFrame = CGRectMake(0, 0, 150, 150);
+    
+    UIImageView *testImgView = [[UIImageView alloc] initWithFrame: testImgFrame];
+    
+    testImgView.backgroundColor = [UIColor greenColor];
+    
+    [_gameView addSubview: testImgView];
 }
 
+- (void) arrayMakeAction {
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    imgsArr = [[NSArray alloc] initWithObjects:
+               @"barbara.jpg",
+               @"c37e471cbda190a9c8cce3892d3fda26.jpg",
+               @"charlie_1_20160614_1804687380.jpg",
+               @"ciwHbm-L.jpg",
+               @"colour_blocks.png",
+               @"cropped-image-17.jpg",
+               @"dream-image.jpg",
+               @"Image Essentials Stetson.jpg",
+               @"image-3-512x512.jpg",
+               @"image.jpg",
+               @"Lichtenstein_img_processing_test.png",
+               @"noImg.png",
+               @"on_the_phone.jpg",
+               @"peppers.png",
+               @"Snapshot _ Roby  Coccy  IRDS 101 180 22 - Adulti.png",
+               @"Snapshot _ Roby  Coccy  IRDS 123 224 22 - Adulti.png",
+               @"Superdomo-la-rioja-image.jpg",
+               @"texture.jpg",
+               @"zaneplay_sm.jpg", nil];
+}
+
+- (IBAction)resetFourAction:(id)sender {
+    
+}
+
+- (IBAction)resetSixAction:(id)sender {
+    
 }
 
 
